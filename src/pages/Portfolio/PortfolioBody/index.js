b7a2cb02be469e1f3541932749edaf9386f91840
hoangth <tranhuyhoang8496@gmail.com>
2022-12-30 00:15:37 +0700

    dev: the portfolio has been done

--- src/pages/Portfolio/PortfolioBody/index.js
+++ src/pages/Portfolio/PortfolioBody/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { onGenerateIcon } from '../../../utils/utilities';
 
 const PortfolioBody = ({ data }) => {
@@ -9,20 +9,66 @@ const PortfolioBody = ({ data }) => {
       <div className='main-column'>
         {data
           .filter((item) => item.icon)
-          .map((item) => {
+          .map((item, index) => {
             return (
-              <div>
-                {onGenerateIcon(item.icon)}
-                <span>{item.header}</span>
-              </div>
+              <Fragment key={index}>
+                <div className='main-header'>
+                  {onGenerateIcon(item.icon)}
+                  <span>{item.header}</span>
+                </div>
+                <div className='main-body' key={index}>
+                  <ol>
+                    {item.body.map((bodyItem, bodyIndex) => {
+                      return (
+                        <Fragment key={bodyIndex}>
+                          {bodyItem.title ? (
+                            <li key={bodyIndex}>
+                              <p>{bodyItem.title}</p>
+                              <span>{bodyItem.subtitle}</span>
+                              <ul>
+                                {bodyItem.content.map((contentItem, contentIndex) => {
+                                  return <li key={contentIndex}>{contentItem}</li>;
+                                })}
+                              </ul>
+                            </li>
+                          ) : (
+                            <ul>
+                              {bodyItem.content.map((contentItem, contentIndex) => {
+                                return <li key={contentIndex}>{contentItem}</li>;
+                              })}
+                            </ul>
+                          )}
+                        </Fragment>
+                      );
+                    })}
+                  </ol>
+                </div>
+              </Fragment>
             );
           })}
       </div>
       <div className='side-column'>
         {data
           .filter((item) => !item.icon)
-          .map((item) => {
-            return <div>{item.header}</div>;
+          .map((item, index) => {
+            return (
+              <div className='side-item' key={index}>
+                <div className='side-header'>{item.header}</div>
+                <div className='side-body'>
+                  {item.body.map((bodyItem, bodyIndex) => {
+                    return (
+                      <Fragment key={bodyIndex}>
+                        <div>{bodyItem.title}</div>
+                        <div>{bodyItem.subtitle}</div>
+                        {bodyItem.content.map((contentItem, contentIndex) => {
+                          return <div key={contentIndex}>{contentItem}</div>;
+                        })}
+                      </Fragment>
+                    );
+                  })}
+                </div>
+              </div>
+            );
           })}
       </div>
     </div>
